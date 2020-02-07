9904f913799701bc8100249bd620e469d5b6064e
Rustem Nasyrov <nasyrov.rustem.r@ya.ru>
2020-02-07 10:55:33 +0500

    Added configuration

---
+++ gulpfile.js
@@ -0,0 +1,77 @@
+'use strict';
+
+const {
+    src,
+    dest,
+    series,
+    parallel,
+    watch
+} = require('gulp'),
+    autoprefixer = require('gulp-autoprefixer'),
+    webpack = require('webpack-stream'),
+    sass = require('gulp-sass'),
+    sourcemaps = require('gulp-sourcemaps'),
+    imagemin = require('gulp-imagemin');
+
+const path = {
+    src: {
+        base: './src/*.js',
+        css: './src/resources/styles/**/*.{css,scss,css}',
+        img: './src/resources/images/**/*.*',
+    },
+    dist: {
+        base: 'C:/ws/www/ws5/lib/formsbuilder',
+        css: 'C:/ws/www/ws5/lib/formsbuilder/res/css',
+        img: 'C:/ws/www/ws5/lib/formsbuilder/res/img',
+    },
+};
+
+const base = () => {
+    return src(path.src.base)
+        .pipe(webpack({
+            output: {
+                filename: 'formsConstructor.min.js',
+                library: 'FormsConstructor',
+            },
+            devtool: 'source-map',
+            mode: 'production',
+            optimization: {
+                minimize: true,
+            },
+            module: {
+                rules: [{
+                    test: /\.js$/,
+                    exclude: /node_modules/,
+                    use: ['babel-loader'],
+                }],
+            },
+        }))
+        .pipe(dest(path.dist.base));
+}
+
+const css = cb => {
+    return src(path.src.css)
+        .pipe(sourcemaps.init())
+        .pipe(sass({
+            outputStyle: 'compressed',
+        }).on('error', (error) => cb(error.message)))
+        .pipe(autoprefixer({
+            cascade: false,
+        }))
+        .pipe(sourcemaps.write('./'))
+        .pipe(dest(path.dist.css))
+}
+
+const img = () => {
+    return src(path.src.img)
+        .pipe(imagemin())
+        .pipe(dest(path.dist.img));
+}
+
+const watcher = () => {
+    watch(path.src.base, base, base);
+    watch(path.src.css, css, css);
+    watch(path.src.img, img, img);
+}
+
+exports.default = series(parallel(base, css, img), watcher);
