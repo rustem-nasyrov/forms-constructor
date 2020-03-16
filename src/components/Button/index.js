fae66d8d511f277c3d2e4e6bc305548571cc9b19
Rustem Nasyrov <nasyrov.rustem.r@ya.ru>
2020-03-16 16:23:52 +0500

    Writed event handler methods

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -1,14 +1,31 @@
 class Button {
+    // private variables
     #outerHTML = '';
+    // public variables
     constructor(options) {
-        let { type = null, value = 'Default' } = options;
+        let { type = null, value = 'Default', handler = null, renderTo } = options;
+        if (!renderTo) throw new Error(`"renderTo" does not specified!`);
         let buttonElem = document.createElement('button');
         if (type) buttonElem.setAttribute('type', type);
         buttonElem.innerText = value;
+        if (handler) this.attachEvent({ elem: buttonElem, type: 'click', func: handler });
         this.#outerHTML = buttonElem.outerHTML;
+        document.querySelector(renderTo).appendChild(buttonElem);
     }
     get html() {
         return this.#outerHTML;
+    }
+    // Methods
+    // Event setter
+    attachEvent(eventConfig) {
+        let { type = null, func = null, elem = null } = eventConfig;
+        this[`on${type}`] = func;
+        elem && type && func ? elem.addEventListener(type, func) : '';
+    }
+    detachEvent(eventConfig) {
+        let { type = null, func = null, elem = null } = eventConfig;
+        this[`on${type}`] = null;
+        elem && type && func ? elem.removeEventListener(type, func) : '';
     }
 }
 export default Button;
