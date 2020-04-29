3c4b572015f6296817ebe1a923cb782b5d8f01b9
Rustem Nasyrov <nasyrov.rustem.r@ya.ru>
2020-04-29 18:02:56 +0500

    Rewrited field config.

--- src/components/Field/index.js
+++ src/components/Field/index.js
@@ -6,17 +6,28 @@ export default class Field {
     parentElem = null;
     elem = null;
     constructor(elem, options) {
-        console.group('Field');
-        console.log(elem);
-        console.log(options);
-        console.groupEnd('Field');
         let fieldElem = document.createElement('input');
         // let fieldContainer
         let { type = 'text', label = null } = options;
+        let fieldElemContainer = document.createElement('div');
+
         this.parentElem = elem;
         this.elem = fieldElem;
+        let fieldsCount = document.querySelectorAll('input').length;
+        this.elem.id = `fc-field-${fieldsCount}-${type}`;
+        fieldElemContainer.id = `fc-field-${fieldsCount}`;
         fieldElem.type = type;
-        // if (label) new Label(this.elem, label);
-        this.parentElem.appendChild(fieldElem);
+
+        this.applyExistingOptionsToElem(options);
+        fieldElemContainer.appendChild(this.elem);
+        if (label) new Label(this.elem, label);
+        this.parentElem.appendChild(fieldElemContainer);
+    }
+    applyExistingOptionsToElem(options) {
+        for (let option in options) {
+            if (typeof this.elem[option] !== 'undefined') {
+                this.elem[option] = options[option];
+            }
+        }
     }
 }
