d36bb20cfafdeb9d6c7bdafd4f2a0de71433884c
Rustem Nasyrov <nasyrov.rustem.r@ya.ru>
2020-04-29 16:22:55 +0500

    Working on Field component

--- src/components/Field/index.js
+++ src/components/Field/index.js
@@ -1,7 +1,22 @@
 'use strict';
 
+import Label from '../Label';
+
 export default class Field {
-    constructor() {
-        console.log('Field');
+    parentElem = null;
+    elem = null;
+    constructor(elem, options) {
+        console.group('Field');
+        console.log(elem);
+        console.log(options);
+        console.groupEnd('Field');
+        let fieldElem = document.createElement('input');
+        // let fieldContainer
+        let { type = 'text', label = null } = options;
+        this.parentElem = elem;
+        this.elem = fieldElem;
+        fieldElem.type = type;
+        // if (label) new Label(this.elem, label);
+        this.parentElem.appendChild(fieldElem);
     }
 }
